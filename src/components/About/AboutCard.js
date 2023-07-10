ea0e531c07b6e205076cfb5399e609e550c8ead6
RyanLouw <110169633+RyanLouw@users.noreply.github.com>
2023-07-10 13:29:02 +0200

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Ryan Louw </span>
+            from <span className="purple"> Krugersdorp, South Africa.</span>
+            <br />I am A Web/App developer at SAC trucks <br />
+            Additionally, I got my DSc in Information technology from NWU.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Learning New Information Stacks
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Code is like humor. When you have to explain it, it’s bad."{" "}
+          </p>
+          <footer className="blockquote-footer">Cory House</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
